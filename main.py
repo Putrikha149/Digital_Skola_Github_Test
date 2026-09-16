d6bb70b7cee56b883da0e87d9307e66ed6ba69fe
Putrikha149 <putrikhairunisa149@gmail.com>
2026-09-16 22:22:34 +0700

    Add add_new function to enable setting new value for number attribute

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-#Penjumlahan
+#Tugas 3
 def jumlahkan(num_1 : int, num_2 = 10):
   result = num_1 + num_2
   return result
   
 Jumlahkan(num_1) #num_1 diinput merupakan integer
+
+#Tugas 4
+class Angka : 
+  def __init__(self, number: int):
+    self.number = number
+  def add_new(self, other_num:int):
+    new_value = jumlahkan((self.number+other_num), num_2 = 10)
+    return new_value
